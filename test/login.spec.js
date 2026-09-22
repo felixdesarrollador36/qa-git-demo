21a733e1b8c7cf623c38252abaa581907d18a128
Felix Lora <elcristiano9095@gmail.com>
2026-09-22 00:39:39 -0400

    primer commit

---
+++ test/login.spec.js
@@ -0,0 +1,12 @@
+//ejemplo de script
+
+//imprimir mensaje
+
+
+console.log("Mi primer mensaje del login")
+
+console.log("Mi primer mensaje del login 2")
+
+//imprimir mensaje 3
+
+console.log("Mi primer mensaje del login 3")
