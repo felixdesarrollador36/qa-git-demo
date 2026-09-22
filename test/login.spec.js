de483b9a6a640804dded360a521af68da761eae5
Felix Lora <elcristiano9095@gmail.com>
2026-09-22 00:58:29 -0400

    primer commit de la rama del login

--- test/login.spec.js
+++ test/login.spec.js
@@ -15,3 +15,11 @@ console.log("Mi primer mensaje del login 3")
 //imprimir mensaje 4
 
 console.log("Mi primer mensaje del login 4")
+
+//imprimir mensaje 5
+
+console.log("Mi primer mensaje del login 5")
+
+//imprimir mensaje 6
+
+console.log("Mi primer mensaje del login 6")
