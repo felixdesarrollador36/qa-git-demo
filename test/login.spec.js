2d145fbf5d52472ae994200de8626d69042a1298
Felix Lora <elcristiano9095@gmail.com>
2026-09-22 00:42:11 -0400

    segundo  commit

--- test/login.spec.js
+++ test/login.spec.js
@@ -10,3 +10,8 @@ console.log("Mi primer mensaje del login 2")
 //imprimir mensaje 3
 
 console.log("Mi primer mensaje del login 3")
+
+
+//imprimir mensaje 4
+
+console.log("Mi primer mensaje del login 4")
